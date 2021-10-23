5e125d0fcca4b6903d6376e4f5a610bbef76e31c
David Lekve <davidlek@stud.ntnu.no>
2021-10-23 18:45:13 +0200

    working handleliste

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,30 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+List<String> handleliste=<String>[];
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  void _submit(){
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  final _textController= TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    _textController.dispose();
+    super.dispose();
+  }
+
+  void _submit(){
+    setState(() {
+      handleliste.add(_textController.text);
+    });
   }
 
   @override
@@ -21,18 +40,19 @@ class MyApp extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            HandleItem(),
+            HandleListWidget(),
             Row(
               children: [
-                const Flexible(
+                Flexible(
                   child: TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), hintText: 'Legg til'),
-                    //onSubmitted: _submit(value),
+                    controller: _textController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Handlevare'),
+                    onSubmitted: (value)=>_submit(),
                   ),
                 ),
                 TextButton(
-                  onPressed: () {},
+                  onPressed: ()=>_submit(),
                   child: const Icon(Icons.add),
                   style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Colors.white)),
                 )
@@ -45,20 +65,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HandleItem extends StatefulWidget {
+
+//HandleItemWidget
+
+class HandleItemWidget extends StatefulWidget {
+  String text;
+
+  HandleItemWidget({Key? key,required this.text}):super (key:key);
+
   @override
   State<StatefulWidget> createState() {
-    return HandleItemState();
+    return HandleItemWidgetState(text);
   }
 }
 
-class HandleItemState extends State<HandleItem> {
+class HandleItemWidgetState extends State<HandleItemWidget> {
+  String text;
+
+  HandleItemWidgetState(this.text);
+
   bool? _checked = false;
 
   @override
   Widget build(BuildContext context) {
     return CheckboxListTile(
-      title: const Text('400g Kjøttdeig'),
+      title: Text(text),
       value: _checked,
       onChanged: (bool? value) {
         setState(() {
@@ -66,6 +97,25 @@ class HandleItemState extends State<HandleItem> {
         });
       },
       activeColor: Colors.green,
+    );
+  }
+}
+
+
+//HandleListWidget
+
+class HandleListWidget extends StatefulWidget {
+  const HandleListWidget({ Key? key }) : super(key: key);
+
+  @override
+  _HandleListWidgetState createState() => _HandleListWidgetState();
+}
+
+class _HandleListWidgetState extends State<HandleListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: handleliste.map((handleitem) => HandleItemWidget(text: handleitem)).toList()
     );
   }
 }
